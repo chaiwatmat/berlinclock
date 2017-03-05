be3e4671021121ac750b1b1a7f72732fb8fbbed9
Chaiwat Matarak <chaiwat.mat.dev@gmail.com>
2017-03-05 20:45:27 +0700

    Initial

---
+++ berlinclock/BerlinClock.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Chaiwatmat.BerlinClock
+{
+    public class BerlinClock
+    {
+        private DateTime _currentDateTime;
+
+        public BerlinClock(DateTime? datetime)
+        {
+            _currentDateTime = datetime ?? DateTime.Now;
+        }
+
+        public DateTime CurrentTime
+        {
+            get{
+                return _currentDateTime;
+            }
+            set{
+                _currentDateTime = DateTime.Now;
+            }
+        }
+
+        public string GetTopClock(){
+            return _currentDateTime.Second % 2 == 0 ? "Y" : "O";
+        }
+
+        public string GetTopHoursClock(){
+            var signals = "";
+            var hourQuarters = (int)Math.Floor(_currentDateTime.Hour / 5d);
+
+            signals = signals.PadLeft(hourQuarters, 'Y');
+            signals = signals.PadRight(4, 'O');
+
+            return signals;
+        }
+
+        public string GetBottomHoursClock(){
+            var signals = "";
+            var hourQuarters = _currentDateTime.Hour % 5;
+
+            signals = signals.PadLeft(hourQuarters, 'Y');
+            signals = signals.PadRight(4, 'O');
+
+            return signals;
+        }
+
+
+
+        public string GetTopMinutesClock(){
+            var signals = "";
+            var minuteRanges = (int)Math.Floor(_currentDateTime.Minute / 5d);
+
+            for(var i = 1; i <= minuteRanges; i++){
+                signals += i % 3 == 0 ? "R" : "Y";
+            }
+            signals = signals.PadRight(11, 'O');
+
+            return signals;
+        }
+
+        public string GetBottomMinutesClock(){
+            var signals = "";
+            var minuteRanges = _currentDateTime.Minute % 5;
+
+            signals = signals.PadLeft(minuteRanges, 'Y');
+            signals = signals.PadRight(4, 'O');
+
+            return signals;
+        }
+    }
+}
---
+++ test/BerlinClockTest.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using NUnit.Framework;
+using Chaiwatmat.BerlinClock;
+
+namespace Chaiwatmat.BerlinClock.Test
+{
+    // R = Red
+    // Y = Yellow
+    // O = Off
+
+    [TestFixture]
+    public class BerlinClockTest
+    {
+        private BerlinClock _berlinClock;
+
+        [TestCase("Y")]
+        public void WhenSecondsIsEven_TopClockShouldYellow(string expected){
+            var datetime = new DateTime(2017, 1, 1, 0, 0, 0);
+
+            _berlinClock = new BerlinClock(datetime);
+            var result = _berlinClock.GetTopClock();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("O")]
+        public void WhenSecondsIsNotEven_TopClockShouldOff(string expected){
+            var datetime = new DateTime(2017, 1, 1, 0, 0, 59);
+
+            _berlinClock = new BerlinClock(datetime);
+            var result = _berlinClock.GetTopClock();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("OOOO")]
+        public void WhenHoursMidNight_TopHoursClockShouldAllOff(string expected){
+            var datetime = new DateTime(2017, 1, 1, 0, 0, 0);
+
+            _berlinClock = new BerlinClock(datetime);
+            var result = _berlinClock.GetTopHoursClock();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("YYYY")]
+        public void WhenHours11Pm_TopHoursClockShouldBeYYYY(string expected){
+            var datetime = new DateTime(2017, 1, 1, 23, 59, 59);
+
+            _berlinClock = new BerlinClock(datetime);
+            var result = _berlinClock.GetTopHoursClock();
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestCase("YYOO")]
+        public void WhenHours2Am_BottomHoursClockShouldYYOO(string expected){
+            var datetime = new DateTime(2017, 1, 1, 2, 0, 0);
+
+            _berlinClock = new BerlinClock(datetime);
+            var result = _berlinClock.GetBottomHoursClock();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("OOOO")]
+        public void WhenHours5Am_BottomHoursClockShouldOOOO(string expected){
+            var datetime = new DateTime(2017, 1, 1, 5, 0, 0);
+
+            _berlinClock = new BerlinClock(datetime);
+            var result = _berlinClock.GetBottomHoursClock();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("YYYY")]
+        public void WhenHours4Am_BottomHoursClockShouldYYYY(string expected){
+            var datetime = new DateTime(2017, 1, 1, 4, 0, 0);
+
+            _berlinClock = new BerlinClock(datetime);
+            var result = _berlinClock.GetBottomHoursClock();
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestCase("OOOOOOOOOOO")]
+        public void WhenMinutesZero_TopMinutesClockShouldAllOff(string expected){
+            var datetime = new DateTime(2017, 1, 1, 0, 0, 0);
+
+            _berlinClock = new BerlinClock(datetime);
+            var result = _berlinClock.GetTopMinutesClock();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("YYOOOOOOOOO")]
+        public void WhenMinutesTen_TopMinutesClockFirst2ShouldYY(string expected){
+            var datetime = new DateTime(2017, 1, 1, 0, 10, 0);
+
+            _berlinClock = new BerlinClock(datetime);
+            var result = _berlinClock.GetTopMinutesClock();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("YYROOOOOOOO")]
+        public void WhenMinutesNineteen_TopMinutesClockShouldStartWithYYROOO(string expected){
+            var datetime = new DateTime(2017, 1, 1, 0, 19, 0);
+
+            _berlinClock = new BerlinClock(datetime);
+            var result = _berlinClock.GetTopMinutesClock();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("YYRYYRYYRYY")]
+        public void WhenMinutesFiftyNine_TopMinutesClockShouldYYRYYRYYRYY(string expected){
+            var datetime = new DateTime(2017, 1, 1, 0, 59, 0);
+
+            _berlinClock = new BerlinClock(datetime);
+            var result = _berlinClock.GetTopMinutesClock();
+            Assert.AreEqual(expected, result);
+        }
+
+
+
+        [TestCase("OOOO")]
+        public void WhenMinutesZero_BottomMinutesClockShouldOff(string expected){
+            var datetime = new DateTime(2017, 1, 1, 0, 0, 0);
+
+            _berlinClock = new BerlinClock(datetime);
+            var result = _berlinClock.GetBottomMinutesClock();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("YYYY")]
+        public void WhenMinutesZero_BottomMinutesClockShouldAllY(string expected){
+            var datetime = new DateTime(2017, 1, 1, 0, 59, 0);
+
+            _berlinClock = new BerlinClock(datetime);
+            var result = _berlinClock.GetBottomMinutesClock();
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
