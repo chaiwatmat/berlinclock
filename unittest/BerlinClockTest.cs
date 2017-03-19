d7756c3623ce0b2a7966a7006918f4eb9c1cda84
Chaiwat Matarak <chaiwat.mat.dev@gmail.com>
2017-03-19 22:25:58 +0700

    migrate to 1.0.1 and change nunit to xunit

--- unittest/BerlinClockTest.cs
+++ unittest/BerlinClockTest.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using NUnit.Framework;
+using Xunit;
 using Chaiwatmat.BerlinClock;
 
 namespace Chaiwatmat.BerlinClock.Test
@@ -8,130 +8,142 @@ namespace Chaiwatmat.BerlinClock.Test
     // Y = Yellow
     // O = Off
 
-    [TestFixture]
     public class BerlinClockTest
     {
         private BerlinClock _berlinClock;
 
-        [TestCase("Y")]
+        [Theory]
+        [InlineData("Y")]
         public void WhenSecondsIsEven_TopClockShouldYellow(string expected){
             var datetime = new DateTime(2017, 1, 1, 0, 0, 0);
 
             _berlinClock = new BerlinClock(datetime);
             var result = _berlinClock.GetTopClock();
-            Assert.AreEqual(expected, result);
+            Assert.Equal(expected, result);
         }
 
-        [TestCase("O")]
+        [Theory]
+        [InlineData("O")]
         public void WhenSecondsIsNotEven_TopClockShouldOff(string expected){
             var datetime = new DateTime(2017, 1, 1, 0, 0, 59);
 
             _berlinClock = new BerlinClock(datetime);
             var result = _berlinClock.GetTopClock();
-            Assert.AreEqual(expected, result);
+            Assert.Equal(expected, result);
         }
 
-        [TestCase("OOOO")]
+        [Theory]
+        [InlineData("OOOO")]
         public void WhenHoursMidNight_TopHoursClockShouldAllOff(string expected){
             var datetime = new DateTime(2017, 1, 1, 0, 0, 0);
 
             _berlinClock = new BerlinClock(datetime);
             var result = _berlinClock.GetTopHoursClock();
-            Assert.AreEqual(expected, result);
+            Assert.Equal(expected, result);
         }
 
-        [TestCase("YYYY")]
+        [Theory]
+        [InlineData("YYYY")]
         public void WhenHours11Pm_TopHoursClockShouldBeYYYY(string expected){
             var datetime = new DateTime(2017, 1, 1, 23, 59, 59);
 
             _berlinClock = new BerlinClock(datetime);
             var result = _berlinClock.GetTopHoursClock();
-            Assert.AreEqual(expected, result);
+            Assert.Equal(expected, result);
         }
 
 
-        [TestCase("YYOO")]
+        [Theory]
+        [InlineData("YYOO")]
         public void WhenHours2Am_BottomHoursClockShouldYYOO(string expected){
             var datetime = new DateTime(2017, 1, 1, 2, 0, 0);
 
             _berlinClock = new BerlinClock(datetime);
             var result = _berlinClock.GetBottomHoursClock();
-            Assert.AreEqual(expected, result);
+            Assert.Equal(expected, result);
         }
 
-        [TestCase("OOOO")]
+        [Theory]
+        [InlineData("OOOO")]
         public void WhenHours5Am_BottomHoursClockShouldOOOO(string expected){
             var datetime = new DateTime(2017, 1, 1, 5, 0, 0);
 
             _berlinClock = new BerlinClock(datetime);
             var result = _berlinClock.GetBottomHoursClock();
-            Assert.AreEqual(expected, result);
+            Assert.Equal(expected, result);
         }
 
-        [TestCase("YYYY")]
+        [Theory]
+        [InlineData("YYYY")]
         public void WhenHours4Am_BottomHoursClockShouldYYYY(string expected){
             var datetime = new DateTime(2017, 1, 1, 4, 0, 0);
 
             _berlinClock = new BerlinClock(datetime);
             var result = _berlinClock.GetBottomHoursClock();
-            Assert.AreEqual(expected, result);
+            Assert.Equal(expected, result);
         }
 
 
-        [TestCase("OOOOOOOOOOO")]
+        [Theory]
+        [InlineData("OOOOOOOOOOO")]
         public void WhenMinutesZero_TopMinutesClockShouldAllOff(string expected){
             var datetime = new DateTime(2017, 1, 1, 0, 0, 0);
 
             _berlinClock = new BerlinClock(datetime);
             var result = _berlinClock.GetTopMinutesClock();
-            Assert.AreEqual(expected, result);
+            Assert.Equal(expected, result);
         }
 
-        [TestCase("YYOOOOOOOOO")]
+        [Theory]
+        [InlineData("YYOOOOOOOOO")]
         public void WhenMinutesTen_TopMinutesClockFirst2ShouldYY(string expected){
             var datetime = new DateTime(2017, 1, 1, 0, 10, 0);
 
             _berlinClock = new BerlinClock(datetime);
             var result = _berlinClock.GetTopMinutesClock();
-            Assert.AreEqual(expected, result);
+            Assert.Equal(expected, result);
         }
 
-        [TestCase("YYROOOOOOOO")]
+        [Theory]
+        [InlineData("YYROOOOOOOO")]
         public void WhenMinutesNineteen_TopMinutesClockShouldStartWithYYROOO(string expected){
             var datetime = new DateTime(2017, 1, 1, 0, 19, 0);
 
             _berlinClock = new BerlinClock(datetime);
             var result = _berlinClock.GetTopMinutesClock();
-            Assert.AreEqual(expected, result);
+            Assert.Equal(expected, result);
         }
 
-        [TestCase("YYRYYRYYRYY")]
+        [Theory]
+        [InlineData("YYRYYRYYRYY")]
         public void WhenMinutesFiftyNine_TopMinutesClockShouldYYRYYRYYRYY(string expected){
             var datetime = new DateTime(2017, 1, 1, 0, 59, 0);
 
             _berlinClock = new BerlinClock(datetime);
             var result = _berlinClock.GetTopMinutesClock();
-            Assert.AreEqual(expected, result);
+            Assert.Equal(expected, result);
         }
 
 
 
-        [TestCase("OOOO")]
+        [Theory]
+        [InlineData("OOOO")]
         public void WhenMinutesZero_BottomMinutesClockShouldOff(string expected){
             var datetime = new DateTime(2017, 1, 1, 0, 0, 0);
 
             _berlinClock = new BerlinClock(datetime);
             var result = _berlinClock.GetBottomMinutesClock();
-            Assert.AreEqual(expected, result);
+            Assert.Equal(expected, result);
         }
 
-        [TestCase("YYYY")]
+        [Theory]
+        [InlineData("YYYY")]
         public void WhenMinutesZero_BottomMinutesClockShouldAllY(string expected){
             var datetime = new DateTime(2017, 1, 1, 0, 59, 0);
 
             _berlinClock = new BerlinClock(datetime);
             var result = _berlinClock.GetBottomMinutesClock();
-            Assert.AreEqual(expected, result);
+            Assert.Equal(expected, result);
         }
 
     }
